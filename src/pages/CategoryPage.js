6baba24e2d3706ce974f7303e5994177ffc8190e
Wynspeare <wynspeare@gmail.com>
2019-03-15 16:23:11 -0500

    Basic HomePage and CategoriesPage built

---
+++ src/pages/CategoryPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CategoryPage extends Component {
+  render() {
+    return (
+      <div> 
+        {/* <h1> Wine Page </h1> */}
+      </div>
+    );
+  }
+}
+
+export default CategoryPage;
